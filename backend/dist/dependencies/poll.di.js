bbf513a2ac1a88490c424c761e77cc990d154325
Razirasheed03 <razirasheed03@gmail.com>
2026-02-04 21:09:14 +0530

    chore: ignore backend build output and system files

--- backend/dist/dependencies/poll.di.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pollController = exports.pollService = void 0;
-const poll_controller_1 = require("../controllers/implements/poll.controller");
-const poll_repository_1 = require("../repositories/implements/poll.repository");
-const poll_service_1 = require("../services/implements/poll.service");
-exports.pollService = new poll_service_1.PollService(new poll_repository_1.PollRepository());
-exports.pollController = new poll_controller_1.PollController(exports.pollService);
-//# sourceMappingURL=poll.di.js.map
